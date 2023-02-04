aef454c56183c3aca546b452ef82cd85129e635f
Jorge Alberto Diaz Orozco (Akiel) <diazorozcoj@gmail.com>
2023-02-04 18:17:52 +0100

    Add instructions to build debian package to goreleaser

--- completions.sh
+++ completions.sh
@@ -4,6 +4,6 @@ set -e
 rm -rf completions
 mkdir completions
 for sh in bash zsh fish; do
-	go run main.go completion "$sh" >"completions/jwt-decode-claims.$sh"
-	chmod +x completions/jwt-decode-claims.$sh
+	go run main.go completion "$sh" >"completions/jwt-decode.$sh"
+	chmod +x completions/jwt-decode.$sh
 done
